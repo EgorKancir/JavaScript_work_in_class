938526aef48e6b72b75a7e8e4ab7454efa531d56
EgorKancir <kancyregor@gmail.com>
2025-04-11 15:05:08 +0200

    16_HTTP_AJAX

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-alert("Hello");
+import axios from 'axios';
+// async - ця функція буде асинхронною
+const getImgs = async () => {
+  // const img = fetch(
+  //   'https://pixabay.com/api/?key=37133631-8f51e9c6f744fc90bf0fea549&q=yellow+flowers&image_type=photo'
+  // );
+  // console.log(img);
+  // img
+  //   .then(value => {
+  //     return value.json();
+  //   })
+  //   .then(date => {
+  //     console.log(date);
+  //   });
+
+  // async await
+  // try catch
+
+    try {
+        const { data } = await axios.get(
+            'https://pixabay.com/api/?key=37133631-8f51e9c6f744fc90bf0fea549&q=yellow+flowers&image_type=photo'
+        );
+        console.log(data.hits);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+getImgs();
